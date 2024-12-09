e1560dbd6f3be7cc0bb93d168fc8120c13bdcf48
KUIJEN <91477681+Kuijen@users.noreply.github.com>
2024-12-09 13:37:23 +0100

    Bumpoed to V2.5.5

--- START.cmd
+++ START.cmd
@@ -1,8 +1,8 @@
 REM Kills Virtual Desktop when the script starts or resets
 :start
 @echo off
- tasklist | findstr "Virtual Desktop Streamer" 
- taskkill /F /IM VirtualDesktop.Streamer.exe
+ tasklist >NUL | findstr "Virtual Desktop Streamer" 
+ taskkill >NUL /F /IM VirtualDesktop.Streamer.exe
  cls
 )
 REM Displays the Logo
@@ -11,7 +11,7 @@ REM Displays the Logo
  @echo[
  @echo[
  @echo[
- @echo off & Title RT-RP V2.5 Beta 3
+ @echo off & Title RT-RP V2.5.5
  echo/  /$$$$$$$  /$$$$$$$$      /$$$$$$$  /$$$$$$$ 
  echo/ ^| $$__  $$^|__  $$__/     ^| $$__  $$^| $$__  $$
  echo/ ^| $$  \ $$   ^| $$        ^| $$  \ $$^| $$  \ $$
@@ -19,21 +19,26 @@ REM Displays the Logo
  echo/ ^| $$__  $$   ^| $$^|______/^| $$__  $$^| $$____/ 
  echo/ ^| $$  \ $$   ^| $$        ^| $$  \ $$^| $$      
  echo/ ^| $$  ^| $$   ^| $$        ^| $$  ^| $$^| $$      
- echo/ ^|__/  ^|__/   ^|__/  V2.5  ^|__/  ^|__/^|__/      
- echo/                                              
- echo/          Copyright 2024 By KUIJEN                                            
+ echo/ ^|__/  ^|__/   ^|__/ V2.5.5 ^|__/  ^|__/^|__/      
+ echo/                                        
+ echo/                        
  echo/                                              
  echo/ 
  echo/ 
  @echo off
 )
 
-set "config=config.txt"
+REM Defines the Backup Config location as "cfgbk"
+set "cfgbk=Text Files\Backup Config.txt"
 
-REM Checks if the config file exists
+REM Defines the Config file location as "config"
+set "config=%userprofile%\Documents\RT-RP Config.txt"
+
+REM Checks if the config file exists and puts it in it's correct place if it isn't
 if not exist "%config%" (
-    echo Confic file not found: %config%
-    pause
+    copy >NUL "%cfgbk%" "%config%"
+    echo [4;43;30mConfig File Not Found, Copied New Config To Documents.[0m
+    echo.
 )
 
 REM Reads and processes each line in config.txt
@@ -48,8 +53,6 @@ for /f "usebackq tokens=1,2 delims==" %%A in ("%config%") do (
     )
 )
 
-
-
 REM Define the path to check for vrmonitor.exe (SteamVR)
 set "steamvrpath=C:\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win64\vrmonitor.exe"
 
@@ -61,7 +64,7 @@ IF %ERRORLEVEL% EQU 0 (
 )
 
 REM Check if SteamVR should launch
-if "%LaunchSteamVR%"=="1" (
+if "%Launch_SteamVR%"=="1" (
     REM Check if vrmonitor.exe exists at the specified path
     IF EXIST "%steamvrpath%" (
         start "" "%steamvrpath%"
@@ -76,7 +79,7 @@ if "%LaunchSteamVR%"=="1" (
 :res1
 
 REM Define the path to check for Amethyst.exe
-set "amethystPath=C:\Program Files\WindowsApps\11835K2VRTeam.Amethyst-OpenSourceBodyTracking_1.2.14.0_x64__g2vc4cfdbyb66\Amethyst.exe"
+set "amethystPath=%Amethyst%"
 
 REM Check if Amethyst.exe is running
 tasklist /FI "IMAGENAME eq Amethyst.exe" 2>NUL | find /I "Amethyst.exe" >NUL
@@ -86,7 +89,7 @@ IF %ERRORLEVEL% EQU 0 (
 )
 
 REM Check if Amethyst should launch
-if "%LaunchAmethyst%"=="1" (
+if "%Launch_Amethyst%"=="1" (
     REM Check if Amethyst.exe exists at the specified path
     IF EXIST "%amethystPath%" (
         start "" "%amethystPath%"
@@ -101,7 +104,7 @@ if "%LaunchAmethyst%"=="1" (
 :res2
 
 REM Define the path to check for slimevr.exe
-set "slimevrPath=C:\Program Files (x86)\SlimeVR Server\slimevr.exe"
+set "slimevrPath=%SlimeVR%"
 
 REM Check if slimevr.exe is running
 tasklist /FI "IMAGENAME eq slimevr.exe" 2>NUL | find /I "slimevr.exe" >NUL
@@ -111,7 +114,7 @@ IF %ERRORLEVEL% EQU 0 (
 )
 
 REM Check if SlimeVR should launch
-if "%LaunchSlimeVR%"=="1" (
+if "%Launch_SlimeVR%"=="1" (
     REM Check if slimevr.exe exists at the specified path
     IF EXIST "%slimevrPath%" (
         start "" "%slimevrPath%"
@@ -126,7 +129,7 @@ if "%LaunchSlimeVR%"=="1" (
 :res3
 
 REM Define the path to check for VRCFaceTracking.exe
-set "VRCFTPath=C:\Program Files\WindowsApps\96ba052f-0948-44d8-86c4-a0212e4ae047_5.2.3.0_x64__4s4k90pjvq32p\VRCFaceTracking.exe"
+set "VRCFTPath=%VRCFT%"
 
 REM Check if VRCFaceTracking.exe is running
 tasklist /FI "IMAGENAME eq VRCFaceTracking.exe" 2>NUL | find /I "VRCFaceTracking.exe" >NUL
@@ -136,7 +139,7 @@ IF %ERRORLEVEL% EQU 0 (
 )
 
 REM Check if VRCFT should launch
-if "%LaunchVRCFT%"=="1" (
+if "%Launch_VRCFT%"=="1" (
     REM Check if VRCFaceTracking.exe exists at the specified path
     IF EXIST "%VRCFTPath%" (
         start "" "%VRCFTPath%"
@@ -151,7 +154,7 @@ if "%LaunchVRCFT%"=="1" (
 :res4
 
 REM Define the path to check for VirtualDesktop.Streamer.exe
-set "VDPath=C:\Program Files\Virtual Desktop Streamer\VirtualDesktop.Streamer.exe"
+set "VDPath=%Virtual_Desktop%"
 
 REM Check if VirtualDesktop.Streamer.exe is running
 tasklist /FI "IMAGENAME eq VirtualDesktop.Streamer.exe" 2>NUL | find /I "VirtualDesktop.Streamer.exe" >NUL
@@ -161,7 +164,7 @@ IF %ERRORLEVEL% EQU 0 (
 )
 
 REM Check if Virtual Desktop should launch
-if "%LaunchVirtualDesktop%"=="1" (
+if "%Launch_Virtual_Desktop%"=="1" (
     REM Check if VirtualDesktop.Streamer.exe exists at the specified path
     IF EXIST "%VDPath%" (
         start "" "%VDPath%"
@@ -175,25 +178,24 @@ if "%LaunchVirtualDesktop%"=="1" (
 
 :res5
 
-REM Define the path to check for ALVR Dashboard.exe
-REM MAKE PATH CONFIGURABLE IN CONFIG FILE!!! User should put file path here for now.
-set "ALVRPath="
+REM Define the path to check for VHConsole.exe (Vive Hub)
+set "VIVEpath=%Vive_Hub%"
 
-REM Check if ALVR Dashboard.exe is running
-tasklist /FI "IMAGENAME eq ALVR Dashboard.exe" 2>NUL | find /I "ALVR Dashboard" >NUL
+REM Check if VHConsole.exe is running
+tasklist /FI "IMAGENAME eq VHConsole.exe" 2>NUL | find /I "VHConsole.exe" >NUL
 IF %ERRORLEVEL% EQU 0 (
-    echo alvr is [92mAlready Running![0m
+    echo Vive Hub is [92mAlready Running![0m
     goto :res6
 )
 
-REM Check if ALVR should launch
-if "%LaunchALVR%"=="1" (
-    REM Check if ALVR Dashboard.exe exists at the specified path
-    IF EXIST "%ALVRPath%" (
-        start "" "%ALVRPath%"
-        echo ALVR [92mStarted![0m
+REM Check if Vive Hub should launch
+if "%Launch_VIVE_HUB%"=="1" (
+    REM Check if VHConsole.exe exists at the specified path
+    IF EXIST "%VIVEpath%" (
+        start "" "%VIVEpath%"
+        echo Vive Hub [92mStarted![0m
     ) ELSE (
-        echo ALVR Was [33mNot[0m Found!
+        echo Vive Hub Was [33mNot[0m Found!
     )
 ) ELSE (
     goto :res6
@@ -201,17 +203,77 @@ if "%LaunchALVR%"=="1" (
 
 :res6
 
-@echo[
-@echo[
+REM Define the path to check for ALVR Dashboard.exe
+set "ALVRpath=%ALVR%"
+
+REM Check if ALVR Dashboard.exe is running
+tasklist /FI "IMAGENAME eq ALVR Dashboard.exe" 2>NUL | find /I "ALVR Dashboard.exe" >NUL
+IF %ERRORLEVEL% EQU 0 (
+    echo ALVR is [92mAlready Running![0m
+    goto :res7
+)
+
+REM Check if ALVR should launch
+if "%Launch_ALVR%"=="1" (
+    REM Check if ALVR Dashboard.exe exists at the specified path
+    IF EXIST "%ALVRpath%" (
+        start "" "%ALVRpath%"
+        echo ALVR [92mStarted![0m
+    ) ELSE (
+        echo ALVR Was [33mNot[0m Found!
+    )
+) ELSE (
+    goto :res7
+)
+
+:res7
+
+REM Define the path to check for VRCX.exe
+set "VRCXpath=%VRCX%"
+
+REM Check if VRCX.exe is running
+tasklist /FI "IMAGENAME eq VRCX.exe" 2>NUL | find /I "VRCX.exe" >NUL
+IF %ERRORLEVEL% EQU 0 (
+    echo VRCX is [92mAlready Running![0m
+    goto :res8
+)
+
+REM Check if VRCX should launch
+if "%Launch_VRCX%"=="1" (
+    REM Check if VRCX.exe exists at the specified path
+    IF EXIST "%VRCXpath%" (
+        start "" "%VRCXpath%"
+        echo VRCX [92mStarted![0m
+    ) ELSE (
+        echo VRCX Was [33mNot[0m Found!
+    )
+) ELSE (
+    goto :res8
+)
+
+:res8
+
+echo.
+echo.
 
 REM Starts gnirehtet, the program responsible for the reverse tether.
 gnirehtet.exe start
-@echo off 
 
-REM Throws an error if gnirehtet fails to run and prompts the user to reset the script
-if errorlevel 1 (echo [4;91mPlease connect your headset and restart RT-RP[0m) & (echo Press Any Key To Restart. . .) & start "" "%~dp0cmdmp3win.exe" "%~dp0\stm\error.mp3" & pause >nul & cls && goto :start
+REM Throws an error if gnirehtet fails to run and prompts the user to reset the script and plays the error sound
+if errorlevel 1 (
+ if %Enable_Sound%==1 (
+    start "" "cmdmp3win.exe" "STM\error.mp3")
+ echo [4;91mPlease connect your headset and restart RT-RP[0m
+ echo Press Any Key To Restart. . .
+ pause >nul
+ cls
+ goto :start
+)
 
-start "" "%~dp0cmdmp3win.exe" "%~dp0\stm\start.mp3"
+REM Plays sound on RT start when set to true in config
+if %Enable_Sound%==1 (
+ start "" "cmdmp3win.exe" "STM\start.mp3"
+)
 
 @java -jar gnirehtet.jar relay
 @pause
